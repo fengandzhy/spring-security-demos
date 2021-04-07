68acd8577620ea25cdaf328f57edeaed5673574b
fengandzhy <fengandzhy@gmail.com>
2021-04-08 05:01:35 +1200

    remove unuse projects

--- spring-security-J/spring-security-jpa/src/main/java/org/zhouhy/springsecurity/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package org.zhouhy.springsecurity.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zhouhy.springsecurity.entities.User;
-
-public interface UserDao extends JpaRepository<User,Long> {
-    User findByUsername(String username);
-}
