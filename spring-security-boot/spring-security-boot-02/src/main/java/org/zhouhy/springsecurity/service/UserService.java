a6f85a89f43501e994946eba049d6c795e757452
fengandzhy <fengandzhy@gmail.com>
2021-04-08 04:57:26 +1200

    remove the unuse project

--- spring-security-boot/spring-security-boot-02/src/main/java/org/zhouhy/springsecurity/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package org.zhouhy.springsecurity.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-}
