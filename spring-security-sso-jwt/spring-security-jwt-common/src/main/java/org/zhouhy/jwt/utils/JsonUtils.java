f22e3d361f529e8fd14010156e114a46f24581fd
fengandzhy <fengandzhy@gmail.com>
2021-07-03 12:57:16 +1200

    jsonutil

--- spring-security-sso-jwt/spring-security-jwt-common/src/main/java/org/zhouhy/jwt/utils/JsonUtils.java
+++ spring-security-sso-jwt/spring-security-jwt-common/src/main/java/org/zhouhy/jwt/utils/JsonUtils.java
@@ -1,9 +1,15 @@
 package org.zhouhy.jwt.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class JsonUtils {
     
@@ -26,4 +32,39 @@ public class JsonUtils {
         }
     }
     
+    public static <T> T toBean(String json,Class<T> eClass){
+        try {
+            return mapper.readValue(json,eClass);
+        } catch (IOException e) {
+            logger.error("json解析出错：" + json, e);
+            return null;            
+        }
+    }
+
+    public static <E> List<E> toList(String json, Class<E> eClass){
+        try {
+            return mapper.readValue(json,mapper.getTypeFactory().constructCollectionType(List.class, eClass));
+        } catch (IOException e) {
+            logger.error("json解析出错：" + json, e);
+            return null;
+        }
+    }
+
+    public static <K,V> Map<K,V> toMap(String json, Class<K> kClass,Class<V> vClass){
+        try {
+            return mapper.readValue(json,mapper.getTypeFactory().constructMapType(Map.class,kClass,vClass));
+        } catch (IOException e) {
+            logger.error("json解析出错：" + json, e);
+            return null;
+        }
+    }
+
+    public static <T> T nativeRead(String json, TypeReference<T> type){
+        try {
+            return mapper.readValue(json, type);
+        } catch (IOException e) {
+            logger.error("json解析出错：" + json, e);
+            return null;
+        }
+    }
 }
