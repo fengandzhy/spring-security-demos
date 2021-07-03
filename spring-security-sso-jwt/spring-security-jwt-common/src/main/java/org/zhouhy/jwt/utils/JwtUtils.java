6e0fdf008cd4019a0ef8e7b8eb9d95eb07dea658
fengandzhy <fengandzhy@gmail.com>
2021-07-03 13:09:23 +1200

    JwtUtils

---
+++ spring-security-sso-jwt/spring-security-jwt-common/src/main/java/org/zhouhy/jwt/utils/JwtUtils.java
@@ -0,0 +1,53 @@
+package org.zhouhy.jwt.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.joda.time.DateTime;
+
+import java.security.PrivateKey;
+import java.util.Base64;
+import java.util.UUID;
+
+public class JwtUtils {   
+    public static final String JWT_PAYLOAD_USER_KEY = "user";
+
+    /**
+     * 私钥加密token
+     *
+     * @param userInfo   载荷中的数据
+     * @param privateKey 私钥
+     * @param expire     过期时间，单位分钟
+     * @return JWT
+     */
+    public static String generateTokenExpireInMinutes(Object userInfo, PrivateKey privateKey,int expire){
+        return Jwts.builder()
+                .claim(JWT_PAYLOAD_USER_KEY,JsonUtils.toString(userInfo))
+                .setId(createJTI())
+                .setExpiration(DateTime.now().plusMinutes(expire).toDate())
+                .signWith(privateKey, SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    /**
+     * 私钥加密token
+     *
+     * @param userInfo   载荷中的数据
+     * @param privateKey 私钥
+     * @param expire     过期时间，单位秒
+     * @return JWT
+     */
+    public static String generateTokenExpireInSeconds(Object userInfo, PrivateKey privateKey,int expire){
+        return Jwts.builder()
+                .claim(JWT_PAYLOAD_USER_KEY,JsonUtils.toString(userInfo))
+                .setId(createJTI())
+                .setExpiration(DateTime.now().plusSeconds(expire).toDate())
+                .signWith(privateKey, SignatureAlgorithm.ES256)
+                .compact();
+    }
+    
+    
+    
+    private static String createJTI(){
+        return new String(Base64.getEncoder().encode(UUID.randomUUID().toString().getBytes()));
+    }
+}
