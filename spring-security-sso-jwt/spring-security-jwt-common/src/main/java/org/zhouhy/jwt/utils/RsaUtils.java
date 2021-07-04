c08c70fbf6b27f0b69adfe936768f2972b5def17
fengandzhy <fengandzhy@gmail.com>
2021-07-04 13:42:25 +1200

    RsaUtil

--- spring-security-sso-jwt/spring-security-jwt-common/src/main/java/org/zhouhy/jwt/utils/RsaUtils.java
+++ spring-security-sso-jwt/spring-security-jwt-common/src/main/java/org/zhouhy/jwt/utils/RsaUtils.java
@@ -1,6 +1,86 @@
 package org.zhouhy.jwt.utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 public class RsaUtils {
     private static final int DEFAULT_KEY_SIZE=2048;
+
+    /**
+     * 从文件中读取公钥
+     *
+     * @param filename 公钥保存路径，相对于classpath
+     * @return 公钥对象
+     * @throws Exception
+     */
+    public static PublicKey getPublicKey(String filename) throws Exception{
+        byte[] bytes = readFile(filename);
+        return getPublicKey(bytes);
+    }
     
+    public static PrivateKey getPrivateKey(String filename)throws Exception{
+        byte[] bytes = readFile(filename);
+        return getPrivateKey(bytes);
+    }
+    
+    public static void generateKey(String publicKeyFilename,String privateKeyFilename,
+                                   String secret,int keySize)throws Exception{
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        SecureRandom secureRandom = new SecureRandom(secret.getBytes());
+        keyPairGenerator.initialize(Math.max(keySize, DEFAULT_KEY_SIZE), secureRandom);
+        KeyPair keyPair = keyPairGenerator.genKeyPair();
+        // 获取公钥并写出
+        byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
+        publicKeyBytes = Base64.getEncoder().encode(publicKeyBytes);
+        writeFile(publicKeyFilename, publicKeyBytes);
+        // 获取私钥并写出
+        byte[] privateKeyBytes = keyPair.getPrivate().getEncoded();
+        privateKeyBytes = Base64.getEncoder().encode(privateKeyBytes);
+        writeFile(privateKeyFilename, privateKeyBytes); 
+    }
+
+    /**
+     * 获取公钥
+     *
+     * @param bytes 公钥的字节形式
+     * @return
+     * @throws Exception
+     */
+    private static PublicKey getPublicKey(byte[] bytes)throws Exception{
+        bytes = Base64.getDecoder().decode(bytes);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(bytes);
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        return factory.generatePublic(spec);
+    }
+
+    /**
+     * 获取密钥
+     *
+     * @param bytes 私钥的字节形式
+     * @return
+     * @throws Exception
+     */
+    private static PrivateKey getPrivateKey(byte[] bytes)throws Exception{
+        bytes = Base64.getDecoder().decode(bytes);
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(bytes);
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        return factory.generatePrivate(spec);
+    }
+    
+    private static byte[] readFile(String filename)throws Exception{
+        return Files.readAllBytes(new File(filename).toPath());
+    }
+    
+    private static void writeFile(String destPath,byte[] bytes)throws IOException {
+        File dest = new File(destPath);
+        if(!dest.exists()){
+            dest.createNewFile();
+        }
+        Files.write(dest.toPath(),bytes);
+    }
 }
