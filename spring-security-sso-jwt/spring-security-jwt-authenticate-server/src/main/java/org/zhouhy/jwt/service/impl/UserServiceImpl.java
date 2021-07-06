4eab5b66ec54cc2dcf614e7c09dc206e30d7b993
fengandzhy <fengandzhy@gmail.com>
2021-07-06 16:57:54 +1200

    userService

--- spring-security-sso-jwt/spring-security-jwt-authenticate-server/src/main/java/org/zhouhy/jwt/service/impl/UserServiceImpl.java
+++ spring-security-sso-jwt/spring-security-jwt-authenticate-server/src/main/java/org/zhouhy/jwt/service/impl/UserServiceImpl.java
@@ -1,12 +1,21 @@
 package org.zhouhy.jwt.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.zhouhy.jwt.domain.UserPojo;
+import org.zhouhy.jwt.mapper.UserMapper;
 import org.zhouhy.jwt.service.UserService;
 
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper mapper;
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        UserPojo userPojo = mapper.queryByUserName(username);
+        return userPojo;
     }
 }
