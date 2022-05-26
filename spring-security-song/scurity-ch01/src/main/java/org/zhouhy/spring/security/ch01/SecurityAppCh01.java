8dfb7f1a95345f7ece44e803e1a680142040c23f
fengandzhy <fengandzhy@gmail.com>
2022-05-26 17:12:08 +1200

    spring security songge

---
+++ spring-security-song/scurity-ch01/src/main/java/org/zhouhy/spring/security/ch01/SecurityAppCh01.java
@@ -0,0 +1,11 @@
+package org.zhouhy.spring.security.ch01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAppCh01 {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityAppCh01.class);
+    }
+}
