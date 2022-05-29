de873b064c7b822098c28e54052e97e841b6348f
fengandzhy <fengandzhy@gmail.com>
2022-05-29 14:57:12 +1200

    spring security song

--- spring-security-song/security-ch02/src/main/java/org/zhouhy/spring/security/ch02/config/SecurityConfig.java
+++ spring-security-song/security-ch02/src/main/java/org/zhouhy/spring/security/ch02/config/SecurityConfig.java
@@ -7,14 +7,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * 这个类必须继承于WebSecurityConfigurerAdapter, 并且有@Configuration 注解 
+ * 
+ * */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
+    // 这是个密码加密的bean
     @Bean
     PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
 
+    // 设置用户名和密码的
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
                 auth.inMemoryAuthentication()
