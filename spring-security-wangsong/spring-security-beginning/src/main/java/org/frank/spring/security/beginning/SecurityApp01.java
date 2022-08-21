0e1c2b1e6bd00d707e30cd66b5f091ff66f18ad1
fengandzhy <fengandzhy@gmail.com>
2022-08-22 09:23:46 +1200

    spring-security-beginning controllers

---
+++ spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/SecurityApp01.java
@@ -0,0 +1,12 @@
+package org.frank.spring.security.beginning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApp01 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApp01.class,args);
+    }
+}
---
+++ spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/controller/HelloController.java
@@ -0,0 +1,14 @@
+package org.frank.spring.security.beginning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name){
+        return "hello," + name;
+    }
+}
