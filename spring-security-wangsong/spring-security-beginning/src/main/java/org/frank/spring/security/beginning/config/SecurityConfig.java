9490e4c5094c156907d18b621400fea2f551406a
fengandzhy <fengandzhy@gmail.com>
2022-08-25 21:33:46 +1200

    security beginning update SecurityConfig.java

--- spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/config/SecurityConfig.java
+++ spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/config/SecurityConfig.java
@@ -1,8 +1,24 @@
 package org.frank.spring.security.beginning.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
     
-    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("frank")
+                .password("654321").roles("admin");
+    }
 }
