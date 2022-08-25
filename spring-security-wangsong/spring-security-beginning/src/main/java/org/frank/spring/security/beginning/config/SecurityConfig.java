b5c3d26b947bccea3a60fd7a03b33f8cd9eb65ca
fengandzhy <fengandzhy@gmail.com>
2022-08-25 21:51:15 +1200

    security beginning add some comments in SecurityConfig.java

--- spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/config/SecurityConfig.java
+++ spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/config/SecurityConfig.java
@@ -10,11 +10,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * 这是个密码加密的方法, 它会创建一个实现了PasswordEncoder接口的类的实例, 并把它注入到容器中
+     * 让这个实例去给用户的密码加密
+     * */
     @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
     
+    /**
+     * 这是一个用户产生方式的类 
+     * */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
