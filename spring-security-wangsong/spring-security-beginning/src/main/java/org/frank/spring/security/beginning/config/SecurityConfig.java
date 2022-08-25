58d12c946bc2a5016c87784f387aa7fc95361484
fengandzhy <fengandzhy@gmail.com>
2022-08-25 21:28:16 +1200

    security beginning add SecurityConfig.java

---
+++ spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package org.frank.spring.security.beginning.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    
+}
