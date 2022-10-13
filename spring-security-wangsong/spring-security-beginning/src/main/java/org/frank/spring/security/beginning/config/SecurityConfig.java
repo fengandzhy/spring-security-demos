72924593942d9914824561b5dc050c4eaa9f4317
fengandzhy <fengandzhy@gmail.com>
2022-10-14 09:50:30 +1300

    test

--- spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/config/SecurityConfig.java
+++ spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/config/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
-
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
