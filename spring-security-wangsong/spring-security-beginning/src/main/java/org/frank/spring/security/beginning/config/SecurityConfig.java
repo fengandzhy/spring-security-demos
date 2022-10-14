c9896a5b925975849bf4a8977927de516238aa54
fengandzhy <fengandzhy@gmail.com>
2022-10-14 21:14:26 +1300

    security-beginning login.html

--- spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/config/SecurityConfig.java
+++ spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package org.frank.spring.security.beginning.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,19 +15,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * 这是个密码加密的方法, 它会创建一个实现了PasswordEncoder接口的类的实例, 并把它注入到容器中
      * 让这个实例去给用户的密码加密
-     * */
+     */
     @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
-    
+
     /**
-     * 这是一个用户产生方式的类 
-     * */
+     * 这是一个用户产生方式的类
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("frank")
                 .password("654321").roles("admin");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    /**
+     *  1.在Security中.loginPage("/login.html") 这里如果写成这样,并且在controller里面没有配置过/login.html这个路径,
+     *  那么它会以访问静态资源的方式来找到login.html, 那么也就是说在login.html里面一些关于thymeleaf的标签就不起作用了.  
+     *  
+     * */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login.html")
+                    .permitAll()
+                .and()
+                    .csrf().disable();
     }
 }
