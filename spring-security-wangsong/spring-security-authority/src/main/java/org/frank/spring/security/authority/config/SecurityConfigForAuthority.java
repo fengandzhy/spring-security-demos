c7a6277cef2f3ee76ad0cba70f67cf777016225f
fengandzhy <fengandzhy@gmail.com>
2022-10-31 09:56:22 +1300

    security-authority add authority to config

--- spring-security-wangsong/spring-security-authority/src/main/java/org/frank/spring/security/authority/config/SecurityConfigForAuthority.java
+++ spring-security-wangsong/spring-security-authority/src/main/java/org/frank/spring/security/authority/config/SecurityConfigForAuthority.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.io.PrintWriter;
@@ -27,9 +30,19 @@ public class SecurityConfigForAuthority extends WebSecurityConfigurerAdapter {
                 .password("654321").roles("admin");
     }
 
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("frank").password("123").roles("admin").build());
+        manager.createUser(User.withUsername("sam").password("123").roles("user").build());
+        return manager;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("admin")
+                .antMatchers("/user/**").hasRole("user")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -54,7 +67,7 @@ public class SecurityConfigForAuthority extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                .logoutUrl("/logout")
                 .logoutSuccessHandler((req, resp, authentication) -> {
                     resp.setContentType("application/json;charset=utf-8");
                     PrintWriter out = resp.getWriter();
