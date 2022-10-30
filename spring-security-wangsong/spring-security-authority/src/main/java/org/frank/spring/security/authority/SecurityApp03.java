82a3b28d83a4ce3eaea18e806e81c56bcdf5a76b
fengandzhy <fengandzhy@gmail.com>
2022-10-31 09:50:04 +1300

    security-authority config and controller

--- spring-security-wangsong/spring-security-authority/src/main/java/org/frank/spring/security/authority/SecurityApp03.java
+++ spring-security-wangsong/spring-security-authority/src/main/java/org/frank/spring/security/authority/SecurityApp03.java
@@ -1,4 +1,11 @@
 package org.frank.spring.security.authority;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SecurityApp03 {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApp03.class,args);
+    }
 }
---
+++ spring-security-wangsong/spring-security-authority/src/main/java/org/frank/spring/security/authority/config/SecurityConfigForAuthority.java
@@ -0,0 +1,80 @@
+package org.frank.spring.security.authority.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.PrintWriter;
+
+@Configuration
+public class SecurityConfigForAuthority extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("frank")
+                .password("654321").roles("admin");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/doLogin")
+                .usernameParameter("name")
+                .passwordParameter("pwd")
+                .successHandler((req, resp, authentication) -> {
+                    Object principal = authentication.getPrincipal();
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(principal));
+                    out.flush();
+                    out.close();
+                })
+                .failureHandler((req, resp, e) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(e.getMessage());
+                    out.flush();
+                    out.close();
+                })
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                .logoutSuccessHandler((req, resp, authentication) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write("logout success!");
+                    out.flush();
+                    out.close();
+                })
+                .deleteCookies()
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .permitAll()
+                .and()
+                .csrf().disable().exceptionHandling()
+                .authenticationEntryPoint((req, resp, authException) -> {
+                            resp.setContentType("application/json;charset=utf-8");
+                            PrintWriter out = resp.getWriter();
+                            out.write("you have not login, please login!");
+                            out.flush();
+                            out.close();
+                        }
+                );
+    }
+}
