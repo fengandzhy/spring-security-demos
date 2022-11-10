a9d68ed36058eb92e1381ef1742eaf8bd7e56e27
fengandzhy <fengandzhy@gmail.com>
2022-11-10 21:01:58 +1300

    security-jdbc

---
+++ spring-security-wangsong/spring-security-jdbc/src/main/java/org/frank/spring/security/jdbc/config/SecurityConfigForJdbc.java
@@ -0,0 +1,8 @@
+package org.frank.spring.security.jdbc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfigForJdbc extends WebSecurityConfigurerAdapter {
+}
---
+++ spring-security-wangsong/spring-security-jdbc/src/main/java/org/frank/spring/security/jdbc/controller/HelloController.java
@@ -0,0 +1,23 @@
+package org.frank.spring.security.jdbc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/admin/hello")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/user/hello")
+    public String user() {
+        return "user";
+    }
+}
