dfa2baecb8a2b5c3faf48fa876a285704b2b54a2
fengandzhy <fengandzhy@gmail.com>
2022-12-10 18:22:23 +1300

    sprel

--- spring-security-wangsong/spring-security-authority/src/main/java/org/frank/spring/security/authority/controller/HelloController.java
+++ spring-security-wangsong/spring-security-authority/src/main/java/org/frank/spring/security/authority/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @PreAuthorize("principal.username.equals('frank')")
     @GetMapping("/hello")
     public String hello() {
         return "hello";
