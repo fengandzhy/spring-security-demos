de5eacd6fc6c402429828cda8a74f1cf5ef279d5
fengandzhy <fengandzhy@gmail.com>
2022-12-11 16:34:15 +1300

    security-spring-data entities

---
+++ spring-security-wangsong/spring-security-spring-data/src/main/java/org/frank/spring/security/spring/data/SpringDataApp.java
@@ -0,0 +1,11 @@
+package org.frank.spring.security.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataApp {    
+    public static void main(String[] args){
+        SpringApplication.run(SpringDataApp.class,args);
+    }
+}
