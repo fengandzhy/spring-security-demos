71b425d49410954c007834102cad5bef6781cea6
fengandzhy <fengandzhy@gmail.com>
2022-12-11 18:41:43 +1300

    security-spring_data page and controller

--- spring-security-wangsong/spring-security-spring-data/src/main/java/org/frank/spring/security/spring/data/config/SecurityConfigForSpringData.java
+++ spring-security-wangsong/spring-security-spring-data/src/main/java/org/frank/spring/security/spring/data/config/SecurityConfigForSpringData.java
@@ -1,7 +1,9 @@
 package org.frank.spring.security.spring.data.config;
 
+import org.frank.spring.security.spring.data.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,8 +11,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfigForSpringData extends WebSecurityConfigurerAdapter {
 
+    private UserService userService;
+    
+    public SecurityConfigForSpringData(UserService userService){
+        this.userService = userService;
+    }
+    
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();        
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+    
+    
 }
---
+++ spring-security-wangsong/spring-security-spring-data/src/main/java/org/frank/spring/security/spring/data/controller/WelcomeController.java
@@ -0,0 +1,55 @@
+package org.frank.spring.security.spring.data.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {   
+
+
+    @RequestMapping(value="/welcome",method={RequestMethod.GET})
+    public String welcome(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        model.addAttribute("username",username);
+        return "/views/welcome";
+    }
+
+    @RequestMapping(value={"","/","/index"})
+    public String index(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        model.addAttribute("username",username);
+        return "redirect:/welcome";
+    }
+
+//    @RequestMapping(value="/userInfo",method={RequestMethod.GET,RequestMethod.POST})
+//    public String userInfo(HttpServletRequest request, Model model){
+//        Principal principal = request.getUserPrincipal();
+//        String username = principal.getName();
+//        model.addAttribute("username",username);
+//        return "/views/userinfo";
+//    }
+
+    @RequestMapping(value="/hello",method={RequestMethod.GET,RequestMethod.POST})
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping(value="/fail",method={RequestMethod.GET,RequestMethod.POST})    
+    public String login(Model model){        
+        model.addAttribute("fail","fail login222");
+        return "/login";
+    }    
+    
+    @RequestMapping(value="/login.html",method={RequestMethod.GET,RequestMethod.POST})
+    public String loginPage(){        
+        return "/login";
+    }   
+}
