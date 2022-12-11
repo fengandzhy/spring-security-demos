d573856a257ac24b18dd0cdc1584d1bf569671d1
fengandzhy <fengandzhy@gmail.com>
2022-12-12 10:01:19 +1300

    security-spring_data config class

--- spring-security-wangsong/spring-security-spring-data/src/main/java/org/frank/spring/security/spring/data/config/SecurityConfigForSpringData.java
+++ spring-security-wangsong/spring-security-spring-data/src/main/java/org/frank/spring/security/spring/data/config/SecurityConfigForSpringData.java
@@ -4,9 +4,12 @@ import org.frank.spring.security.spring.data.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfigForSpringData extends WebSecurityConfigurerAdapter {
@@ -26,6 +29,36 @@ public class SecurityConfigForSpringData extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService);
     }
-    
-    
+
+    /**
+     * 虽然main.css在static/css里面, 但是这里还是要写成/css/** 不能写成"/static/css/**
+     * 因为这个static是静态资源的默认的访问目录
+     * */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/**", "/css/**","/images/**");
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().formLogin()
+                    .loginPage("/login.html")
+                    .loginProcessingUrl("/doLogin")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/welcome")
+                    .failureUrl("/fail")
+                    .permitAll()
+                .and().logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout","POST"))
+                    .deleteCookies()
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .logoutSuccessUrl("/login.html")
+                    .permitAll()
+                .and()
+                    .csrf().disable();
+    }
 }
