7d069cfccd5cd8b26432b2ad67c58c6941c1fa47
fengandzhy <fengandzhy@gmail.com>
2023-02-18 12:09:10 +1300

    security-ch02 update SecurityConfig.java

--- spring-security-song/security-ch02/src/main/java/org/zhouhy/spring/security/ch02/config/SecurityConfig.java
+++ spring-security-song/security-ch02/src/main/java/org/zhouhy/spring/security/ch02/config/SecurityConfig.java
@@ -2,17 +2,15 @@ package org.zhouhy.spring.security.ch02.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-/**
- * 这个类必须继承于WebSecurityConfigurerAdapter, 并且有@Configuration 注解 
- * 
- * */
+
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig {
     
     // 这是个密码加密的bean
     @Bean
@@ -21,11 +19,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     }
 
     // 设置用户名和密码的
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-                auth.inMemoryAuthentication()
-                .withUser("zhouhy")
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//                auth.inMemoryAuthentication()
+//                .withUser("zhouhy")
+//                .password("123456")
+//                .roles("admin");
+//    }
+    
+    /**
+     * spring boot 2.7.1后 WebSecurityConfigurerAdapter 过期, 以上改成以下.
+     * 
+     * */
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("zhouhy")
                 .password("123456")
-                .roles("admin");
+                .roles("admin")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("frank").password("123").roles("admin").build());
+//        manager.createUser(User.withUsername("sam").password("123").roles("user").build());
+//        return manager;
     }
 }
