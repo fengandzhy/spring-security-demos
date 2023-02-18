265e053742d3cc3c1081f59921e283cf1ab7a9c1
fengandzhy <fengandzhy@gmail.com>
2023-02-18 17:43:57 +1300

    security-remember_me controller and config

---
+++ spring-security-wangsong/spring-security-remember-me/src/main/java/org/frank/spring/security/remember/me/RememberMeApp.java
@@ -0,0 +1,12 @@
+package org.frank.spring.security.remember.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberMeApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RememberMeApp.class,args);
+    }
+}
