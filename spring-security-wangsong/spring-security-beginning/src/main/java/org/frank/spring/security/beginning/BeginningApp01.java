a67511eadac8f22153b2870df49d8ded4e6409a4
fengandzhy <fengandzhy@gmail.com>
2023-02-19 00:46:20 +1300

    security-beginning the complete project

--- spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/BeginningApp01.java
+++ spring-security-wangsong/spring-security-beginning/src/main/java/org/frank/spring/security/beginning/BeginningApp01.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityApp01 {
+public class BeginningApp01 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecurityApp01.class,args);
+        SpringApplication.run(BeginningApp01.class,args);
     }
 }
