970e7827f59eddefb867c41c816b117af8ac11d0
fengandzhy <fengandzhy@gmail.com>
2023-02-19 00:52:01 +1300

    security-authority rename the app

--- spring-security-wangsong/spring-security-authority/src/main/java/org/frank/spring/security/authority/AuthorityApp.java
+++ spring-security-wangsong/spring-security-authority/src/main/java/org/frank/spring/security/authority/AuthorityApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityApp03 {
+public class AuthorityApp {
     public static void main(String[] args) {
-        SpringApplication.run(SecurityApp03.class,args);
+        SpringApplication.run(AuthorityApp.class,args);
     }
 }
