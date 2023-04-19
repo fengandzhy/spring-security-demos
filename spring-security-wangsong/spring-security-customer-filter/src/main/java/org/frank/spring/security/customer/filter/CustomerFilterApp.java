7f68cc6f2b230e585e00a46ba943b77e294a5907
fengandzhy <fengandzhy@gmail.com>
2023-04-19 21:27:24 +1200

    security-customer-filter

---
+++ spring-security-wangsong/spring-security-customer-filter/src/main/java/org/frank/spring/security/customer/filter/CustomerFilterApp.java
@@ -0,0 +1,11 @@
+package org.frank.spring.security.customer.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerFilterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerFilterApp.class);
+    }
+}
